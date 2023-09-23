69750b8ba0b8cba4846cbe05bd2ad4d66187272a
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-09-23 15:52:37 -0400

    modified h2 content

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content was added by JavaScript";
+h2.textContent = "A little about me:";
 
 document.querySelector("body").appendChild(h2);
